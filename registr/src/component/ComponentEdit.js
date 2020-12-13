83c617c363bfb503f6f21aa1f8b4aeffdd9a2773
Arman9090 <arman.ananyan.98@mail.ru>
2020-12-13 23:50:11 +0400

    second commit

--- registr/src/component/ComponentEdit.js
+++ registr/src/component/ComponentEdit.js
@@ -1,39 +1,41 @@
-import React, {Component} from 'react'
-import {postEdit} from '../actions/auth' 
+import React, { Component } from 'react'
+import { postEdit } from '../actions/auth'
 
 
-class ComponentEdit extends Component{
-    state={
-        post:{}
+class ComponentEdit extends Component {
+    state = {
+        post: {}
     }
 
 
-     componentDidMount(){
-         postEdit(this.props.match.params.id).then(res=>{
-            if(res.statusText === 'OK'){
-               this.setState({post: res.data[0]})}
+    componentDidMount() {
+        postEdit(this.props.match.params.id).then(res => {
+            if (res.statusText === 'OK') {
+                this.setState({ post: res.data[0] })
             }
-                )
-
+        }
+        )
     }
-    render(){
+
+    render() {
         console.log(this.state.post)
 
-        const {title, author, text}=this.state.post
-        return(
-            
+        const { title, author, text } = this.state.post
+        return (
+
             <div className='regEdit'>
-                  <div className='registrEdit'>
-                        <p>title</p>
-                            <input className='text'value={title}  name='tiltle'  type='text' placeholder='title'></input>
-                         <p>author</p>
-                         <input className='text' value={author}  name='author' type="text" placeholder="auther"></input>
-                         <p>text</p>
-                        <textarea className='text'value={text}  name='text'  type="text" ></textarea>
-                         <br></br>
-                         <button className='textbtn' >Add the post</button>
-                          </div>
-                         </div>
+                <div className='registrEdit'>
+                    <p>title</p>
+                    <input className='text' value={title} name='tiltle' type='text' placeholder='title'></input>
+                    <p>author</p>
+                    <input className='text' value={author} name='author' type="text" placeholder="auther"></input>
+                    <p>text</p>
+                    <textarea className='text' value={text} name='text' type="text" ></textarea>
+                    <br></br>
+                    <button className='textbtn' >Add the post</button>
+
+                </div>
+            </div>
         )
     }
 }
--- registr/src/component/ComponentShow.js
+++ registr/src/component/ComponentShow.js
@@ -1,48 +1,49 @@
-import React, {Component} from 'react'
-import {postShow} from '../actions/auth'
+import React, { Component } from 'react'
+import { postShow } from '../actions/auth'
 
 
-class  ComponentShow extends Component{
-    state={
-        post:{}
+class ComponentShow extends Component {
+    state = {
+        post: {}
     }
 
-    componentDidMount(){
-        postShow(this.props.match.params.id).then(res=>{
-            if(res.statusText === 'OK'){
-                this.setState({post: res.data[0]})}
+    componentDidMount() {
+        postShow(this.props.match.params.id).then(res => {
+            if (res.statusText === 'OK') {
+                this.setState({ post: res.data[0] })
             }
+        }
         )
 
     }
-    render(){
-    
+    render() {
 
-const {title, author, text}=this.state.post
 
-return(
-    <div className='showClass'>
-        
-        <h6>SHOW POST</h6>
-        <div className='showList'>
-            <div className='showItem'>
-                <b>title</b>
-                <p>{title}</p>
+        const { title, author, text } = this.state.post
+
+        return (
+            <div className='showClass'>
+
+                <h6>SHOW POST</h6>
+                <div className='showList'>
+                    <div className='showItem'>
+                        <b>title</b>
+                        <p>{title}</p>
+                    </div>
+                    <div className='showItem'>
+                        <b>author</b>
+                        <p>{author}</p>
+                    </div>
+                    <div className='showItem'>
+                        <b>text</b>
+                        <p>{text}</p>
+                    </div>
+
+
+                </div>
+
             </div>
-            <div className='showItem'>
-                <b>author</b>
-                <p>{author}</p>
-            </div>
-            <div className='showItem'>
-                <b>text</b>
-                <p>{text}</p>
-            </div>
-
-
-        </div>
-        
-    </div>
-)
-}
+        )
+    }
 }
 export default ComponentShow;
